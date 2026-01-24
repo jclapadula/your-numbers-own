e2f5a6e710f96ae1e3e3158151fe9f0c3f1840d9
Juan Lapadula <lapadula.jc@outlook.com>
2026-01-24 22:47:12 +0100

    fix dockerfile for FE

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,10 +3,18 @@ import tailwindcss from "@tailwindcss/vite";
 import { defineConfig, loadEnv } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
-export default defineConfig(({ mode }) => {
+export default defineConfig(({ mode, command }) => {
   process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
 
   return {
     plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
+    resolve:
+      command === "build"
+        ? {
+            alias: {
+              "react-dom/server": "react-dom/server.node",
+            },
+          }
+        : undefined,
   };
 });
